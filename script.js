c5feb40e63f7ad7371b5105e451e88c0da9fa21a
alexandrktl <alexandrktl2@yandex.ru>
2023-03-31 01:48:35 +0300

    глобальная переделка после первой проверки, перед рефакторингом функции добавления карточки

--- script.js
+++ script.js
@@ -1,81 +1,151 @@
-const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close-button');// закрыть попап редактирования профиля
-const profileEditButton = document.querySelector('.profile__edit-button');
-const popupSaveButton = document.querySelector('.popup__save-button')
-const profileName = document.querySelector('.profile__name-text');
-const profileDescriptionText = document.querySelector('.profile__description-text');
-const insertedName = document.querySelector('.popup__input_text_person-name');
-const insertedDescription = document.querySelector('.popup__input_text_description-of-person');
-const likeButtton = document.querySelector('.place-card__like-button'); //**************************************** */
-const addPhotoPopup = document.querySelector('.popup-new-place');
-const popupImagecloseButton = addPhotoPopup.querySelector('.popup-new-place__close-button');// кнопка закрытия попапа добавления карточки
-const imageEditButton = document.querySelector('.profile__add-button');//кнопка открытия попапа добавления карточки
-const addNewCardButton = document.querySelector('.popup-new-place__add-button');//кнопка одобавления карточки в попапе
+
+const popup = document.querySelector('.popup');//попап общий
+const addPhotoPopup = document.querySelector('.popup_type-new-place');//попап создания новой карточки
+const bigImgPopup = document.querySelector('.popup_type-big-image');//попап большой картинки
+const editProfilePopup = document.querySelector('.popup_type-edit-profile');//попап редактирования имени профиля
+
+const popupProfileForms = document.querySelectorAll('.popup__profile-form');//формы-часть попапа для навешивания сабмита
+
 const cardsGrid = document.querySelector('.places-grid');// контейнер для карточек
-const bigImgPopup = document.querySelector('.popup-big-image');//попап большой картинки
-const bigImgPopupCloseButton = bigImgPopup.querySelector('.popup-big-image__close-button'); // кнопка закрытия попапа большой картинки
+
+const nameOfImgText = document.querySelector('.popup_text-new-place-img-name');
+const urlOfCardText = document.querySelector('.popup_text-new-place-img-url');
+
+const profileEditButton = document.querySelector('.profile__edit-button');//кнопка редактирования профиля
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');// кнопкa закрыть попап 
+const popupSaveButton = document.querySelector('.popup__save-button')//кнопка сохранить в попапе редактирования пофиля
+const addCardButton = document.querySelector('.profile__add-card-button');//кнопка открытия попапа добавления карточки
+const addNewCardButton = document.querySelector('.popup__add-button');//кнопка одобавления карточки в попапе
 
 
 
+const insertedName = document.querySelector('.popup__input_text_person-name');//текст-имени в попапе
+const insertedDescription = document.querySelector('.popup__input_text_description-of-person');//текст-описаание в попапе
+const profileName = document.querySelector('.profile__name-text');//текст-имени в профиле
+const profileDescriptionText = document.querySelector('.profile__description-text');//текст-описаание в профиле
 
-//закрыть попап
-function closeEditProfilePopup() {
-    popup.classList.add('popup_transition');
-    popup.classList.toggle('popup_opened');
-    transferTextFromHeader();
-}
-//закрыть попап большой картинки
-function closeBigImgPopup() {
-    bigImgPopup.classList.add('popup_transition');
-    bigImgPopup.classList.toggle('popup_opened');
-}
-//закрыть попап добавления картинки
-function closeAddPhotoPopup() {
-    addPhotoPopup.classList.add('popup_transition');
-    addPhotoPopup.classList.toggle('popup_opened');
-    document.querySelector('.popup-new-place_text_place-name').value = ''; // очищаем поле ввода
-    imageUrl = document.querySelector('.popup-new-place_text_place-img-url').value = ''; // очищаем поле ввода
-}
-//открыть попап
-function openPopup() {
-    popup.classList.toggle('popup_opened');
-    transferTextFromHeader();
-}
-//открыть попап добавления  картинки
-function openAddPhotoPopup() {
-    addPhotoPopup.classList.toggle('popup_opened');
-}
+const initialCards = [
+    {
+        name: 'Mercedes',
+        link: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+        name: 'Audi',
+        link: 'https://images.unsplash.com/photo-1541348263662-e068662d82af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+        name: 'Ferrari ',
+        link: 'https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'BMW',
+        link: 'https://images.unsplash.com/photo-1580273916550-e323be2ae537?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+    },
+    {
+        name: 'Buhanka',
+        link: 'https://cdn.fishki.net/upload/post/2018/02/19/2516245/tn/duptx.jpg'
+    },
+    {
+        name: 'Walksvagen',
+        link: 'https://images.unsplash.com/photo-1616421275384-a4871cf679d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    }
+];
 
 
-//перенос текста из профиля в попап
+//функция перенос текста из профиля в попап 
 function transferTextFromHeader() {
     insertedName.value = (profileName.textContent)
-    insertedDescription.value = (profileDescriptionText.textContent)
+    insertedDescription.value = (profileDescriptionText.textContent);
+}
+
+//функция закрыть любой попап 
+function closePopup(anyPopup) {
+    anyPopup.classList.add('popup_transition');
+    anyPopup.classList.toggle('popup_opened');
 }
 
 
-//перенос текста из попапа в профиль
-function transferTextFromPopup(evt) {
-    evt.preventDefault()
-    profileName.textContent = insertedName.value
-    profileDescriptionText.textContent = insertedDescription.value
-    closeEditProfilePopup()
+//функция перенос текста из попапа в профиль
+function transferTextFromPopup() {
+    profileName.textContent = insertedName.value;
+    profileDescriptionText.textContent = insertedDescription.value;
+    closePopup(editProfilePopup);
+}
 
+//функция открыть любой попап
+function openPopup(whatToOpen) {
+    whatToOpen.classList.toggle('popup_opened');
 }
 
 
-//кнопка закрыть попап
-popupCloseButton.addEventListener('click', closeEditProfilePopup);
-//кнопка закрыть попап добавления картинки
-popupImagecloseButton.addEventListener('click', closeAddPhotoPopup);
-//кнопка закрыть попап большой картинки
-bigImgPopupCloseButton.addEventListener('click', closeBigImgPopup);
-//кнопка открыть попап 
-profileEditButton.addEventListener('click', openPopup);
-//кнопка открыть попап добавления картинки
-imageEditButton.addEventListener('click', openAddPhotoPopup);
-//кнопка сохранить в попапе
+//кнопка закрыть любой попап
+// находим все крестики проекта по универсальному селектору
+popupCloseButtons.forEach((button) => {
+    // находим 1 раз ближайший к крестику попап 
+    const popup = button.closest('.popup');
+    // устанавливаем обработчик закрытия на крестик
+    button.addEventListener('click', () => closePopup(popup));
+});
+
+//отменили обновление страницы для всех сабмитов и очистили поля после использования                  
+popupProfileForms.forEach(function (item) {
+    item.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        evt.target.reset();
+    });
+});
+
+//кнопка сохранить в попапе редактирования профиля                   
 popupSaveButton.addEventListener('click', transferTextFromPopup);
+
+
+//кнопка добавить новую карточку в попаде добавления нов карточки          
+addNewCardButton.addEventListener('click', function () {
+    const cardName = nameOfImgText.value;
+    const imageUrl = urlOfCardText.value;
+    addCard(cardName, imageUrl);
+}
+);
+
+
+//кнопка открыть попап добавления картинки
+addCardButton.addEventListener('click', function () {
+    openPopup(addPhotoPopup);
+});
+
+
+
+//кнопка редактировать профиль 
+profileEditButton.addEventListener('click', function () {
+    openPopup(editProfilePopup), transferTextFromHeader();
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -101,57 +171,52 @@ function addCard(nameFromPopup, urlFromPopup) {
     //нажатие на картинку-открытие попапа БОЛЬШОЙ картинки
     card.querySelector('.place-card__image').addEventListener('click', function (evt) {
         const imgUrl = evt.target.src; // нашли ссылку на именно эту картинку
-        document.querySelector('.popup-big-image').classList.toggle('popup_opened');//нашли и переключили класс для активации попапа
-        const mainImg = document.querySelector('.popup-big-image__main-image');//нашли большую картинку
+        bigImgPopup.classList.toggle('popup_opened');//нашли и переключили класс для активации попапа
+        const mainImg = document.querySelector('.popup__main-photo');//нашли большую картинку
         mainImg.src = imgUrl;//добавили ей ссылку для отображения
-        document.querySelector('.popup-big-image__text').textContent = card.querySelector('.place-card__desctiption-text').textContent;//нашли текст у попапа и заменили его на текст из карточки
+        document.querySelector('.popup_text-big-image').textContent = card.querySelector('.place-card__desctiption-text').textContent;//нашли текст у попапа и заменили его на текст из карточки
 
     })
-    cardsGrid.prepend(card); //вставили карточк в начало
-    closeAddPhotoPopup();
+    cardsGrid.prepend(card); //вставили карточкy в начало
+    closePopup(addPhotoPopup);
 }
 
-document.querySelector('.popup-new-place__profile-form').addEventListener('submit', function (evt) { // вешаем на форму, потомучто ХЗ, сабмиту только так
-    evt.preventDefault();
-    const cardName = document.querySelector('.popup-new-place_text_place-name').value;
-    const imageUrl = document.querySelector('.popup-new-place_text_place-img-url').value;
-    addCard(cardName, imageUrl);
-}
-);
-
-const initialCards = [
-    {
-      name: 'Mercedes',
-      link: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-    },
-    {
-      name: 'Audi',
-      link: 'https://images.unsplash.com/photo-1541348263662-e068662d82af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-    {
-      name: 'Ferrari ',
-      link: 'https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
-    },
-    {
-      name: 'BMW',
-      link: 'https://images.unsplash.com/photo-1580273916550-e323be2ae537?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-    },
-    {
-      name: 'Bugatti',
-      link: 'https://images.unsplash.com/photo-1584902645120-f86567d892b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80'
-    },
-    {
-      name: 'Walksvagen',
-      link: 'https://images.unsplash.com/photo-1616421275384-a4871cf679d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    }
-  ]; 
 
 
-  document.addEventListener('DOMContentLoaded', () => {
-    for (let i=initialCards.length-1;i>=0;i--){
-        addCard(initialCards[i].name , initialCards[i].link);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = initialCards.length - 1; i >= 0; i--) {
+        addCard(initialCards[i].name, initialCards[i].link);
     }
 });
-
-
-
